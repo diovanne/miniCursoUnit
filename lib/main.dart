480a747ff9c8d93690bdb49846f3d9cfe6251ec8
DiovanneSantos <diovanne.santos@gmail.com>
2023-10-13 19:04:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:frases_pernambuques/home.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
